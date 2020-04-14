a7a8cac72b6cb73557354f670b2682fd54522973
Gautam Ramesh <gautamshivramesh@gmail.com>
2020-04-13 20:24:31 -0400

    Initial Commit

---
+++ EasyHangouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyHangouts
+//
+//  Created by Gautam Shiv Ramesh on 4/13/20.
+//  Copyright © 2020 Gautam Shiv Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
