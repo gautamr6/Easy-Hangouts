57fd188b77d28140745ca2d88d15c6048456378b
Gautam Ramesh <gautamshivramesh@gmail.com>
2020-04-23 16:07:35 -0400

    final changes

--- EasyHangouts/ViewController.swift
+++ EasyHangouts/ViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var nameField: UITextField?
     @IBOutlet weak var usernameField: UITextField?
     @IBOutlet weak var passwordField: UITextField?
     
@@ -24,20 +25,70 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        nameField?.text = ""
+        nameField?.placeholder = "Name"
+        
         usernameField?.text = ""
+        usernameField?.placeholder = "Username"
+        
         passwordField?.text = ""
+        passwordField?.placeholder = "Password"
+    }
+    
+    @IBAction func login() {
+        let username: String = usernameField?.text ?? ""
+        let password: String = passwordField?.text ?? ""
+        
+        self.ref.child("users/\(username)/password").observeSingleEvent(of: .value, with: {(snapshot) in
+            if let passValue = snapshot.value as? NSString {
+                if passValue.isEqual(to: password) {
+                    self.performSegue(withIdentifier: "mainsegue", sender: nil)
+                } else {
+                    self.usernameField?.text = ""
+                    self.usernameField?.placeholder = "Username"
+                    self.passwordField?.text = ""
+                    self.passwordField?.placeholder = "Wrong username or password"
+                }
+            } else {
+                self.usernameField?.text = ""
+                self.usernameField?.placeholder = "Username"
+                
+                self.passwordField?.text = ""
+                self.passwordField?.placeholder = "Wrong username or password"
+            }
+        })
+    }
+    
+    @IBAction func signup() {
+        let name: String = nameField?.text ?? ""
+        let username: String = usernameField?.text ?? ""
+        let password: String = passwordField?.text ?? ""
+        
+        self.ref.child("users/\(username)").observeSingleEvent(of: .value, with: {(snapshot) in
+            if snapshot.exists() {
+                self.usernameField?.text = ""
+                self.usernameField?.placeholder = "Username taken"
+                
+                self.passwordField?.text = ""
+                self.passwordField?.placeholder = "Password"
+            } else {
+                let userPost = ["name": name,
+                                "password": password]
+                let childUpdates = ["/users/\(username)": userPost]
+                self.ref.updateChildValues(childUpdates)
+                
+                self.performSegue(withIdentifier: "mainsegue", sender: nil)
+            }
+        })
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         navigationItem.backBarButtonItem = UIBarButtonItem(
         title: "Logout", style: .plain, target: nil, action: nil)
         
-        if segue.destination is HomeController {
-            let vc = segue.destination as? HomeController
-            
-            vc?.ref = self.ref
-            vc?.username = usernameField?.text ?? ""
-        }
+        let vc = segue.destination as? HomeController
+        vc?.ref = self.ref
+        vc?.username = usernameField?.text ?? ""
     }
 }
 
